7fc94a3ce797ca0e686c8eb1dcc4f786f4fa5f5c
Matteo Vitali <trottomv@gmail.com>
2018-06-09 18:47:58 +0200

    filter for date #2

--- tools/admin.py
+++ tools/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
+from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 from .models import Diary
 
 # Register your models here.
 class DiaryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_date', 'diarytype', 'customer', 'title', 'text', 'author',)
+    list_display = ('title', 'diarytype', 'customer', 'text', 'created_date', 'author',)
     search_fields = ('diarytype', 'customer')
-    list_filter = ('diarytype', 'customer', 'created_date', 'author',)
+    list_filter = ('diarytype', 'customer', 'author', ('created_date', DateRangeFilter))
 
 admin.site.register(Diary, DiaryAdmin)
