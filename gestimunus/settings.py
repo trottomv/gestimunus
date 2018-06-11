7547eabbfb47bb1f98f7433af0da9ece7a6c56ab
Matteo Vitali <trottomv@gmail.com>
2018-06-11 08:30:13 +0200

    min static + try logging

--- gestimunus/settings.py
+++ gestimunus/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'settings',
     'tools',
     'rangefilter',
+    'import_export',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -144,8 +145,66 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/gestimunus/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "gestimunus/static/"),
 ]
+
+
+# ## LOG CONFIG
+# # import logging.config
+# # import os
+# # from django.utils.log import DEFAULT_LOGGING
+#
+# # Disable Django's logging setup
+# LOGGING_CONFIG = None
+#
+# LOGLEVEL = os.environ.get('LOGLEVEL', 'info').upper()
+#
+# logging.config.dictConfig({
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'default': {
+#             # exact format is not important, this is the minimum information
+#             'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+#         },
+#         'django.server': DEFAULT_LOGGING['formatters']['django.server'],
+#     },
+#     'handlers': {
+#         # console logs to stderr
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'default',
+#         },
+#         # Add Handler for Sentry for `warning` and above
+#         'sentry': {
+#             'level': 'WARNING',
+#             'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+#         },
+#         'django.server': DEFAULT_LOGGING['handlers']['django.server'],
+#     },
+#     'loggers': {
+#         # default for all undefined Python modules
+#         '': {
+#             'level': 'WARNING',
+#             'handlers': ['console', 'sentry'],
+#         },
+#         # Our application code
+#         'app': {
+#             'level': LOGLEVEL,
+#             'handlers': ['console', 'sentry'],
+#             # Avoid double logging because of root logger
+#             'propagate': False,
+#         },
+#         # Prevent noisy modules from logging to Sentry
+#         'noisy_module': {
+#             'level': 'ERROR',
+#             'handlers': ['console'],
+#             'propagate': False,
+#         },
+#         # Default runserver request logging
+#         'django.server': DEFAULT_LOGGING['loggers']['django.server'],
+#     },
+# })
