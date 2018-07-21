c504c76d97cd705d38ca71435556c6882a3301ce
Matteo Vitali <trottomv@gmail.com>
2018-07-21 17:52:41 +0200

    heroku set

--- gestimunus/settings.py
+++ gestimunus/settings.py
@@ -118,7 +118,7 @@ DATABASES = {
         # 'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=1500)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
