5470c638b34009052f225d327640d72d99918762
Matteo Vitali <trottomv@gmail.com>
2018-07-24 08:09:00 +0200

    min

--- gestimunus/settings.py
+++ gestimunus/settings.py
@@ -118,8 +118,8 @@ DATABASES = {
         # 'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
