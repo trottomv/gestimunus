55967fa0c9ab5f57f7fb9c9fdab5523f70570a97
Matteo Vitali <trottomv@gmail.com>
2018-07-24 08:02:50 +0200

    test heroku

--- gestimunus/settings.py
+++ gestimunus/settings.py
@@ -31,7 +31,7 @@ DEBUG = False
 # DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['*', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Smart Select settings
@@ -118,8 +118,8 @@ DATABASES = {
         # 'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
-db_from_env = dj_database_url.config(conn_max_age=1500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
